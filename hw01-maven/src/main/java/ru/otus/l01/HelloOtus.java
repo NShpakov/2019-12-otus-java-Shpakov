d0cd1ffa5b67afa6c62542876134bae3abea8a71
nshpakov <Shpakov.NN@ya.ru>
2019-12-29 18:07:24 +0300

    hw01-maven | Выполнено домашнее задание

---
+++ hw01-maven/src/main/java/ru/otus/l01/HelloOtus.java
@@ -0,0 +1,20 @@
+package ru.otus.l01;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class HelloOtus {
+	public static void main(String[] args) {
+
+
+		List<String> list = ImmutableList.of("Hello OTUS 1", "Hello OTUS 2", "Hello OTUS 3");
+		print(list);
+
+	}
+
+	public static void print(Object... args) {
+		Arrays.stream(args).forEach(System.out::println);
+	}
+}
